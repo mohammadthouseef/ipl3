b0983e33667500b3e3d546ca2133a3ca2afd2d6e
mohammadthouseef <thouseeftoshi1308.com>
2024-08-28 22:58:46 +0530

    first commit

---
+++ mi/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from mi.views import *
+
+
+urlpatterns=[
+
+path('captain/',captain,name='captain'),
+
+
+]
---
+++ mi/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+def captain(request):
+    return render(request,'mi.html')
+    
